0dcd05d57d8d2b11a68aa06af8be849b575505ff
Marvin Hansen <marvin.hansen@gmail.com>
2023-11-30 19:57:21 +0800

    Added preliminary sigint signal handler.

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,87 @@
-use autometrics::prometheus_exporter;
-use axum::{routing::get, Router};
-use server::{MyJobRunner};
 use std::net::SocketAddr;
-use tonic::transport::Server;
+
+use autometrics::prometheus_exporter;
+use axum::{Router, routing::get};
 use tokio::{
     signal::unix::{signal, SignalKind}, spawn,
     sync::oneshot::{self, Receiver, Sender},
 };
+use tonic::transport::Server as TonicServer;
+
+use server::MyJobRunner;
+
 use crate::server::job::job_runner_server::JobRunnerServer;
 
 mod server;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Set up the exporter to collect metrics
     prometheus_exporter::init();
 
-    let grpc_addr = "127.0.0.1:50051".parse().unwrap();
-    let web_addr: SocketAddr = "127.0.0.1:8080".parse().unwrap();
+    let grpc_addr = "127.0.0.1:50051".parse().expect("Failed to parse gRPC address");
+    let web_addr: SocketAddr = "127.0.0.1:8080".parse().expect("Failed to parse web address");
 
     // gRPC server
-    let grpc_service = JobRunnerServer::new(MyJobRunner::default());
+    let svc = JobRunnerServer::new(MyJobRunner::default());
 
     // Construct health service for gRPC server
     let (mut health_reporter, health_svc) = tonic_health::server::health_reporter();
     health_reporter.set_serving::<JobRunnerServer<MyJobRunner>>().await;
 
+    // Construct sigint signal handler for graceful shutdown
+    let (signal_tx, signal_rx) = signal_channel();
+    spawn(handle_sigterm(signal_tx));
 
-    spawn(async move {
-        Server::builder()
-            .add_service(grpc_service)
-            .add_service(health_svc)
-            .serve(grpc_addr)
-            .await
-            .expect("gRPC server failed");
-    });
+    // Build gRPC server with health service and signal sigint handler
+    let server = TonicServer::builder()
+        .add_service(svc)
+        .add_service(health_svc)
+        .serve_with_shutdown(grpc_addr, async {
+            signal_rx.await.ok();
+        });
 
-    // Web server with Axum
+    // Start gRPC server
+    //
+    // This one probably blocks the subsequence start of the web server.
+    println!("Server listening on {}", grpc_addr);
+    server
+        .await
+        .expect("Failed to start server");
+
+    // Http handler that exposes metrics to Prometheus
     let app = Router::new().route("/", get(handler)).route(
         "/metrics",
         get(|| async { prometheus_exporter::encode_http_response() }),
     );
 
+    // Web server with Axum
+    //
+    // How do I add a graceful shutdown signal handler
+    // that triggers a proper shutdown together with the gRPC server?
     axum::Server::bind(&web_addr)
         .serve(app.into_make_service())
         .await
         .expect("Web server failed");
+
+    Ok(())
 }
 
 async fn handler() -> &'static str {
     "Hello, World!"
+}
+
+
+fn signal_channel() -> (Sender<()>, Receiver<()>) {
+    oneshot::channel()
+}
+
+async fn handle_sigterm(tx: Sender<()>) {
+    let _ = signal(SignalKind::terminate())
+        .expect("failed to install signal handler")
+        .recv()
+        .await;
+
+    println!("SIGTERM received: shutting down");
+    let _ = tx.send(());
 }
